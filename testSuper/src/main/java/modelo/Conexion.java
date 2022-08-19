1bebba30bdc416c04b7d7e5cb883b8d14048ef7f
Juan Pedraza <juan_pedraza@outlook.com>
2022-08-18 19:15:56 -0500

    Test

---
+++ testSuper/src/main/java/modelo/Conexion.java
@@ -0,0 +1,16 @@
+
+package modelo;
+
+/**
+ *
+ * @author juan_pedraza
+ */
+public class Conexion {
+    
+    private final String base = "usuarios";
+    private final String user = "root";
+    private final String password = "123456";
+    private final String url = "jdbc:mysql://localhost:3306/" + base;
+            
+    
+}
